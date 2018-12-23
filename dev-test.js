2f3f8aa12b02a5168f6d77562368221edb76d8d9
Harneet Singh <harneet.singh.ts@gmail.com>
2018-12-23 16:29:05 +0530

    Unit test Added

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,8 @@
-const Block = require('./block');
+const Blockchain = require('./blockchain');
 
-const fooBlock = Block.mineBlock(Block.genesis(),'foo');
-console.log(fooBlock.toString());
+const bc = new Blockchain();
+
+for(let i=0;i<10;i++)
+{
+  console.log(bc.addBlock(`foo ${i}`).toString());
+}
