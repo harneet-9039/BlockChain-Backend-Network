41aed42387dd2e58fc7abe104e84160da6779b34
Harneet Singh <harneet.singh.ts@gmail.com>
2018-12-23 16:08:16 +0530

    Proof of work for miners

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,12 +1,17 @@
 
 const SHA256 = require('crypto-js/SHA256');
+const { DIFFICULTY, MINE_RATE } = require('../config');
+
+
 
 class Block{
-	constructor(timestamp, lastHash, hash, data) {
+	constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
 		this.timestamp = timestamp;
 		this.lastHash = lastHash;
 		this.hash = hash;
 		this.data = data;
+		this.nonce = nonce;
+		this.difficulty = difficulty || DIFFICULTY;
 	}
 
 
@@ -15,29 +20,37 @@ class Block{
 			Timestamp: ${this.timestamp}
 			Last Hash: ${this.lastHash.substring(0,10)}
 			Hash     : ${this.hash.substring(0,10)}
+			Nonce    : ${this.nonce}
+		Difficulty : ${this.difficulty}
 			Data     : ${this.data}`;
 	}
 
 	static genesis() {
-		return new this('Genesis time','-------','fir57-h45h',[]);
+		return new this('Genesis time','-------','fir57-h45h',[],0, DIFFICULTY);
 	}
 
 	static mineBlock(lastBlock, data){
-		const timestamp = Date.now();
+		let hash, timestamp;
 		const lastHash = lastBlock.hash;
-		const hash = Block.hash(timestamp,lastHash,data);
+		let difficulty = lastBlock.difficulty;
+		let nonce = 0;
+		do{
+		nonce++;
+		timestamp = Date.now();
+		hash = Block.hash(timestamp,lastHash,data, nonce);
+	} while(hash.substring(0,DIFFICULTY)!== '0'.repeat(DIFFICULTY));
 
-		return new this(timestamp, lastHash, hash, data);
+		return new this(timestamp, lastHash, hash, data, nonce);
 	}
 
-	static hash(timestamp, lastHash, data)
+	static hash(timestamp, lastHash, data, nonce, difficulty)
 	{
-		return SHA256(`${timestamp}${lastHash}${data}`).toString();
+		return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
 	}
 
 	static blockHash(block){
-		const {timestamp, lastHash, data} = block;
-		return Block.hash(timestamp,lastHash,data);
+		const {timestamp, lastHash, data, nonce, difficulty} = block;
+		return Block.hash(timestamp,lastHash,data, nonce, difficulty);
 	}
 }
 
--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-
+const { DIFFICULTY } = require('../config');
 
 
 describe('Block',()=>{
@@ -15,5 +15,10 @@ describe('Block',()=>{
 
   it('sets the `last hash` to match the hash of the last block',()=>{
     expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it('generates a hash that matches the difficulty',()=>{
+    expect(block.hash.substring(0,DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+    console.log(block.toString());
   });
 });
