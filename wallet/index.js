0aebebf5577be51ee8b489dfbd881cfa1d308865
Harneet Singh <harneet.singh.ts@gmail.com>
2018-12-25 16:53:57 +0530

    Transaction Pool wallet updated

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,6 @@
 const ChainUtil = require('../chain-util');
 const {INITIAL_BALANCE} = require('../config');
-
+const Transaction = require('./transaction');
 class Wallet {
   constructor(){
     this.balance = INITIAL_BALANCE;
@@ -16,6 +16,23 @@ class Wallet {
 
   sign(dataHash){
     return this.keyPair.sign(dataHash);
+  }
+
+  createTransaction(recipient, amount, transactionPool){
+  {
+    if(amount>this.balance)
+    return;
+  }
+
+  let transaction = transactionPool.existingTransaction(this.publicKey);
+  if(transaction){
+    transaction.update(this, recipient, amount);
+  }
+  else{
+    transaction = Transaction.newTransaction(this, recipient, amount);
+    transactionPool.updateOrAddTransaction(transaction);
+  }
+  return transaction;
   }
 }
 
--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -13,6 +13,10 @@ class TransactionPool{
             this.transactions.push(transaction);
         }
     }
+
+    existingTransaction(address){
+        return this.transactions.find(t=>t.input.address === address);
+    }
 }
 
 module.exports = TransactionPool;
